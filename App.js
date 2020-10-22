36a3e133eecd7e47a6ff3f67778672ab532e0c4c
Leeroy Phili <leeroyphili@gmail.com>
2020-10-21 20:27:27 -0400

    navigation setup

--- App.js
+++ App.js
@@ -7,8 +7,23 @@ import {
       } from 'react-navigation-stack';
 import AccountScreen from './src/screens/AccountScreen';
 import SigninScreen from './src/screens/SigninScreen';
-import SingupScreen from './src/screens/SignupScreen';
+import SignupScreen from './src/screens/SignupScreen';
 import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
+import SignupScreen from "./src/screens/SignupScreen";
 
+const switchNavigator = createSwitchNavigator ({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen
+  })
+})
